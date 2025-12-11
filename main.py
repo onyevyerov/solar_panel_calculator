1ea5f3b0f2108ed81fa89c9388bb2beadfb7cb46
Oleg Nyevyerov <onyevyerov@gmail.com>
2025-12-12 00:33:37 +0100

    implement load_panels_from_file() and main() methods in main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import json
+from typing import List
+
+from source.domain import Panel, Point
+from source.service import SolarPanelCalculator
+
+
+def load_panels_from_file(path: str) -> List[Panel]:
+    """
+    Load Panel data from a JSON file and return Panel objects.
+
+    Args:
+        path (str): Path to a JSON file containing a list of Panel data.
+
+    Returns:
+        list[Panel]: List of panels -> [Panel(x, y)].
+    """
+    with open(path, "r") as f:
+        data = json.load(f)
+
+    return [Panel(top_left=Point(p["x"], p["y"])) for p in data["panels"]]
+
+
+def main():
+    """
+    Loads panels from file, runs the solar panel calculator, and prints results.
+    """
+    panels = load_panels_from_file("examples/sample_input.json")
+    result = SolarPanelCalculator(panels).calculate()
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
