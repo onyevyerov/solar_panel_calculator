de72d8ff569ad5fc39ac52c6850d169ee26006f5
Oleg Nyevyerov <onyevyerov@gmail.com>
2025-12-11 23:58:15 +0100

    Create RafterGrid and implement generate_grid method in rafter.py

---
+++ source/rafter.py
@@ -0,0 +1,39 @@
+from typing import List
+
+from source.config import RAFTER_SPACING
+from source.domain import Panel
+
+
+class RafterGrid:
+    def __init__(self, first_rafter: float = 0.0, spacing: float = RAFTER_SPACING):
+        self.first_rafter = first_rafter
+        self.spacing = spacing
+
+    def generate_grid(self, panels: List[Panel]) -> list[float]:
+        """
+        Return x coordinates of all rafters in the grid. Returns empty list if there are no Panels.
+        """
+        if not panels:
+            return []
+
+        min_x = panels[0].left
+        max_x = panels[0].right
+
+        for panel in panels:
+            if panel.left < min_x:
+                min_x = panel.left
+            if panel.right > max_x:
+                max_x = panel.right
+
+        rafters_x_coordinates = []
+
+        x = self.first_rafter
+
+        while x + self.spacing < min_x:
+            x += self.spacing
+
+        while x <= max_x:
+            rafters_x_coordinates.append(x)
+            x += self.spacing
+
+        return sorted(rafters_x_coordinates)
