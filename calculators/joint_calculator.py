697a99f94114b9bfb1130dd8e8f580379097722b
Oleg Nyevyerov <onyevyerov@gmail.com>
2025-12-13 23:53:25 +0100

    delete _group_panels_into_row() from JoinCalculator. This logic implemented in RowConstructor now

--- calculators/joint_calculator.py
+++ calculators/joint_calculator.py
@@ -1,34 +1,13 @@
 from typing import List
 
 from source.config import JOINT_GAP_THRESHOLD
+from source.constructors.row_constructor import RowConstructor
 from source.domain import Panel, Joint, Point
 
 
 class JointCalculator:
     def __init__(self, panels: List[Panel]):
         self.panels = panels
-
-    def _group_panels_into_row(self) -> List[List[Panel]]:
-        if not self.panels:
-            return []
-
-        sorted_panels = sorted(self.panels, key=lambda p: p.top) # sorting panels by Y
-
-        rows: List[List[Panel]] = []
-        current_row: List[Panel] = [sorted_panels[0]]
-
-        for panel in sorted_panels[1::]:
-            if abs(panel.top - current_row[0].top) <= JOINT_GAP_THRESHOLD:
-                current_row.append(panel)
-            else:
-                current_row.sort(key=lambda p: p.left) # sorting panels by X before adding them to row
-                rows.append(current_row)
-                current_row = [panel]
-
-        current_row.sort(key=lambda p: p.left) # sorting panels by X in last row
-        rows.append(current_row)
-
-        return rows
 
     def _horizontal_joints_in_row(self, row: List[Panel]) -> List[Joint]:
         if not row:
@@ -111,7 +90,8 @@ class JointCalculator:
         """
         Collect all joint in one collection without duplicates. Returns list of Joint or empty list.
         """
-        rows = self._group_panels_into_row()
+        row_constructor = RowConstructor(self.panels)
+        rows = row_constructor.group_panels_into_row()
         if not rows:
             return []
 
