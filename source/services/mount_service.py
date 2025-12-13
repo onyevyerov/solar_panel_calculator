fcd5a0e3e88ecba62afca011dead27220b8fa511
Oleg Nyevyerov <onyevyerov@gmail.com>
2025-12-14 00:48:32 +0100

    add docstrings to MountService(get_mounts_for_panel, get_mounts_for_segment) in mount_service.py

--- source/services/mount_service.py
+++ source/services/mount_service.py
@@ -8,19 +8,40 @@ class MountService:
     def __init__(self, mounts_x_coordinates: List[float]):
         self.mounts_x_coordinates = sorted(float(x) for x in mounts_x_coordinates)
 
-    def get_mounts_for_panel(self, panel: Panel):
+    def get_mounts_for_panel(self, panel: Panel) -> List[float]:
+        """
+        Calculates valid X-coordinates for supports on a single panel.
+        Filters available rafter positions to ensure:
+        1. Rafter alignment.
+        2. Edge Clearance (not closer than 2.0 units) from panel edges.
+
+        Args:
+            panel: Panel to calculate mounts for.
+
+        Returns:
+            List[float]: List of mounts X-coordinates.
+        """
         min_allowed_x = panel.left + EDGE_CLEARANCE
         max_allowed_x = panel.right - EDGE_CLEARANCE
 
         mounts = [
             x for x in self.mounts_x_coordinates
-            # checking if the rafter not closer than 2 units(EDGE_CLEARANCE) to the edge of panel
+            # checking if the mount not closer than 2 units(EDGE_CLEARANCE) to the edge of panel
             if min_allowed_x <= x <= max_allowed_x
         ]
 
         return sorted(mounts)
 
     def get_mounts_for_segment(self, segment: List[Panel]) -> List[float]:
+        """
+        Calculates valid X-coordinates for supports on a segment.
+
+        Args:
+            segment: List of Panels to calculate mounts for.
+
+        Returns:
+            List[float]: List of mounts X-coordinates.
+        """
         mounts: Set[float]= set()
 
         for panel in segment:
