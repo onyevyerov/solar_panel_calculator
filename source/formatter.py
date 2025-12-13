2d280f1f988b0935b07c3fd955e2d8ff585632aa
Oleg Nyevyerov <onyevyerov@gmail.com>
2025-12-14 00:43:29 +0100

    add docstrings to methods of OutputFormatter class in formatter.py

--- source/formatter.py
+++ source/formatter.py
@@ -8,16 +8,28 @@ class OutputFormatter:
 
     @staticmethod
     def mount_to_dict(mount: Mount) -> dict:
+        """
+        Converts a Mount object to a dictionary with rounded 'x' and 'y' coordinates.
+        """
         return {"x": round(mount.position.x, 2), "y": round(mount.position.y, 2)}
 
     @staticmethod
     def joint_to_dict(joint: Joint) -> dict:
+        """
+        Converts a Joint object to a dictionary with rounded 'x' and 'y' coordinates.
+        """
         return {"x": round(joint.position.x, 2), "y": round(joint.position.y, 2)}
 
     @staticmethod
     def mounts_to_list(mounts: Iterable[Mount]) -> List[dict]:
+        """
+        Converts an iterable of Mount objects into a list of dictionaries.
+        """
         return [OutputFormatter.mount_to_dict(m) for m in mounts]
 
     @staticmethod
     def joints_to_list(joints: Iterable[Joint]) -> List[dict]:
+        """
+        Converts an iterable of Joint objects into a list of dictionaries.
+        """
         return [OutputFormatter.joint_to_dict(j) for j in joints]
