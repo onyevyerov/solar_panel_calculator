cfa52029c373d74b5d3d5fea86fa7d7200a82578
Oleg Nyevyerov <onyevyerov@gmail.com>
2025-12-13 23:49:07 +0100

    Write SegmentConstructor in segment_constructor.py

---
+++ source/constructors/segment_constructor.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from source.config import CONTINUOUS_GAP
+from source.constructors.row_constructor import RowConstructor
+from source.domain import Panel
+
+
+class SegmentConstructor:
+    def __init__(self, panels: List[Panel]):
+        self.panels = panels
+
+
+    def group_rows_into_segments(self) -> List[List[Panel]]:
+        if not self.panels:
+            return []
+
+        row_constructor = RowConstructor(self.panels)
+        rows = row_constructor.group_panels_into_row()
+
+        segments: List[List[Panel]] = []
+
+        for row in rows:
+            current_segment = [row[0]]
+
+            for panel_a, panel_b in zip(row, row[1:]):
+                gap = panel_b.left - panel_a.right
+
+                if abs(gap) < CONTINUOUS_GAP:
+                    current_segment.append(panel_b)
+                else:
+                    segments.append(current_segment)
+                    current_segment = [panel_b]
+
+            segments.append(current_segment)
+
+        return segments
