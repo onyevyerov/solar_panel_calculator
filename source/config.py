956f25657d216e118a23129c97907691cc296876
Oleg Nyevyerov <onyevyerov@gmail.com>
2025-12-13 23:46:57 +0100

    add CONTINUOUS_GAP in config.py

--- source/config.py
+++ source/config.py
@@ -5,3 +5,4 @@ EDGE_CLEARANCE = 2.0 # Minimal distance from left/right corner of panel to Suppo
 CANTILEVER_LIMIT = 16.0 # Max distance from panel edge to first/last support(Mount)
 SPAN_LIMIT = 48.0 # Max distance between two consecutive supports(Mounts)
 JOINT_GAP_THRESHOLD = 1.0 # Max horizontal/vertical gap between panel for which Joint is required
+CONTINUOUS_GAP = 1.0
