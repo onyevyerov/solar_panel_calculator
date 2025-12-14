2393b722df1736293fa9ff207b8908cd988f65d2
Oleg Nyevyerov <onyevyerov@gmail.com>
2025-12-14 21:01:20 +0100

    write additional condition if not mounts in CantileverValidator

--- source/validators/cantilever_validator.py
+++ source/validators/cantilever_validator.py
@@ -16,11 +16,19 @@ class CantileverValidator:
         self.cantilevers_limit = cantilevers_limit
 
     def validate(self, segment: List[Panel], mounts: List[float]) -> None:
-        if not segment or not mounts:
+        if not segment:
             return
 
         start_of_segment = segment[0].left
         end_of_segment = segment[-1].right
+
+        if not mounts:
+            if (end_of_segment - start_of_segment) > CANTILEVER_LIMIT:
+                raise CantileverValidatorError(
+                    f"Cantilever exceeded: Segment {start_of_segment} to {end_of_segment}"
+                    f" has no mounts, and full length is > {CANTILEVER_LIMIT}"
+                )
+            return
 
         first_mount = mounts[0]
         last_mount = mounts[-1]
